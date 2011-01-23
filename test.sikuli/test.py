b1a29bb42d827dac2ca65da7dc30d19250dfacd6
Raymond <darkray16@gmail.com>
2011-01-23 16:27:55 +0900

    tests updated

--- test.sikuli/test.py
+++ test.sikuli/test.py
@@ -1,6 +1,2 @@
-check = exists(Pattern("../Images/chat/type_area.png").similar(0.7))
-if check:
-    match = check.getTarget()
-    hover(match)
-else:
-    print("couldn't find it")
+sys.path.append("c:/users/darkray16/desktop\my dropbox\MTGO Bot\")
+import mod
